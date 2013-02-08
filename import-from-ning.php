725356063fd8d83dd27f4871f2cf18a29779d3d9
BronsonQuick <bronson@sennza.com.au>
2013-02-08 19:33:12 +1000

    Removing closing Php tag and bump the version number

--- import-from-ning.php
+++ import-from-ning.php
@@ -4,7 +4,7 @@ Plugin Name: Import from Ning
 Plugin URI: http://teleogistic.net
 Description: Import members and content from a Ning network export into BuddyPress
 Author: Boone Gorges
-Version: 2.0.7
+Version: 2.0.8
 Author URI: http://teleogistic.net
 */
 
@@ -14,7 +14,3 @@ function bp_ning_import_bp_init() {
 	require( dirname( __FILE__ ) . '/bp-functions.php' );
 }
 add_action( 'bp_include', 'bp_ning_import_bp_init' );
-
-
-
-?>

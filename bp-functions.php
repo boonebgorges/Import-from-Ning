6cefba55c814ee606d99149cbf4fabc35bb19eea
BronsonQuick <bronson@sennza.com.au>
2013-02-08 19:35:14 +1000

    Remove the memory usage function

--- bp-functions.php
+++ bp-functions.php
@@ -1,21 +1,6 @@
 <?php
 /* Import from Ning BP functions */
 
-
-function echo_memory_usage() {
-        $mem_usage = memory_get_usage(true);
-
-        if ($mem_usage < 1024)
-            echo $mem_usage." bytes";
-        elseif ($mem_usage < 1048576)
-            echo round($mem_usage/1024,2)." kilobytes";
-        else
-            echo round($mem_usage/1048576,2)." megabytes";
-
-        echo "<br/>";
-    }
-
-
 function bp_ning_import_add_management_pages() {
 	$plugin_page = add_submenu_page( 'bp-general-settings', __('Import from Ning','bp-ning-import'), __('Import from Ning','bp-ning-import'), 'manage_options', __FILE__, 'bp_ning_import_steps' );
 
